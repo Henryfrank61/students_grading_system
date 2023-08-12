58e204c6ef2988399291cf4f0747540f3f3c31eb
Henryfrank <henryfrank61@yahoo.com>
2023-08-12 10:00:49 +0100

    Simple grading sysytem

---
+++ grade.js
@@ -0,0 +1,16 @@
+function conc(){
+    var fname = document.getElementById("first_name").value;
+    var mname = document.getElementById("middle_name").value;
+    var age = document.getElementById("age").value;
+    var icourse = parseInt(document.getElementById("incourse").value);
+    var exam = parseInt(document.getElementById("exam_score").value);
+    var sum = icourse + exam;
+    var newLine = "\r"
+     fname += newLine;
+     mname += newLine;
+     age +=newLine;
+     sum += newLine;
+
+     alert(f )
+   
+}
