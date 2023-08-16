3ee0d5a2ca3e499b9e7a722338cdc1255adecb3f
Henryfrank <henryfrank61@yahoo.com>
2023-08-16 23:59:51 +0100

    added sweetbox

--- grade.js
+++ grade.js
@@ -1,16 +1,15 @@
-function conc(){
-    var fname = document.getElementById("first_name").value;
-    var mname = document.getElementById("middle_name").value;
-    var age = document.getElementById("age").value;
-    var icourse = parseInt(document.getElementById("incourse").value);
-    var exam = parseInt(document.getElementById("exam_score").value);
-    var sum = icourse + exam;
-    var newLine = "\r"
-     fname += newLine;
-     mname += newLine;
-     age +=newLine;
-     sum += newLine;
+// function conc(){
+//     var fname = document.getElementById("first_name").value;
+//     var mname = document.getElementById("middle_name").value;
+//     var age = document.getElementById("age").value;
+//     var icourse = parseInt(document.getElementById("incourse").value);
+//     var exam = parseInt(document.getElementById("exam_score").value);
+//     var sum = icourse + exam;
 
-     alert(f )
-   
-}
+//     alert(fname + mname + age + sum)
+ 
+// }
+let btnshow = document.querySelector('button'); 
+btnshow.addEventListener('click', () => {
+    Swal("Any fool can use a computer");
+});
